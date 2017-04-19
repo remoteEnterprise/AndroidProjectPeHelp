7e89f79a74149f4c9e5f1dfd04144d2fc118776d
KevinFreytas <kevinfreitas14@gmail.com>
2017-04-19 10:50:57 -0300

    Commit das classes Login e Cadastro

--- app/src/main/java/com/example/mathe/pehelp/LoginActivity.java
+++ app/src/main/java/com/example/mathe/pehelp/LoginActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText nlogin;
     private EditText nSenha;
     private Button nBtn;
+    private TextView mCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,13 @@ public class LoginActivity extends AppCompatActivity {
         nlogin = (EditText) findViewById(R.id.login);
         nSenha = (EditText) findViewById(R.id.senha);
         nBtn = (Button) findViewById(R.id.btn);
+        mCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(LoginActivity.this,CadastroActivity.class);
+                startActivity(intent2);
+            }
+        });
 
         nBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,7 +39,7 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    public void logar() {
+    private void logar() {
         String[] users = {"julianajags", "rebecad", "anacarol"};
         String password = "jujutop";
         String user = nlogin.getText().toString();
@@ -45,8 +54,8 @@ public class LoginActivity extends AppCompatActivity {
                 nSenha.setError("Campo vazio!");
                 focus = nSenha;
                 focus.requestFocus();
-            } else if(user.equals(users[x]) && pass.equals(password)){
-                Intent intent = new Intent(this, MenuUsuarioActivity.class);
+            }else if(user.equals(users[x]) && pass.equals(password)){
+                Intent intent = new Intent(this, MenuActivity.class);
                 Bundle bundle = new Bundle();
                 bundle.putString("user", users[x]);
                 intent.putExtras(bundle);
